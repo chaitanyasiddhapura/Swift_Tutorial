ee56bab7922ca62442a6e154bbe768fb6c07d062
Chaitanya Siddhapura <chaitanyasiddhapura06@gmail.com>
2026-03-19 21:50:55 +0530

    Swift First Project

---
+++ 01_Simple_Project/WelcomeIOS/WelcomeIOS/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  WelcomeIOS
+//
+//  Created by vijay on 13/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var lblWelcomeIOS: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
